28aad54f0669d877749d403dae2bd7f9e909e8be
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-12-13 01:52:16 +0530

    added StatusesGet request

--- client.go
+++ client.go
@@ -1,9 +1,57 @@
 package judge0
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"io"
+	"encoding/json"
+
+)
 
 type Client struct {
 	authProvider AuthProvider
 	httpClient   *http.Client
-	baseURL      string
+}
+
+
+
+
+type DashboardSubmission struct {
+	Id       int  `json:"id"`
+	Description string  `json:"description"`
+}
+type DashboardSubmissions []DashboardSubmission
+
+
+func StatusesGet(client Client) (DashboardSubmissions, error)   {
+	judge0Url, err := url.Parse(client.authProvider.GetBaseURL() + "/statuses")
+
+	judgereq, err := http.NewRequest("GET", judge0Url.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing Judge0 URL: %v", err)
+
+	}
+	judgereq.Header.Add("Content-Type", "application/json")
+	judgereq.Header.Add("Authorization", fmt.Sprintf("Bearer %v", bearer))
+
+	resp, err := http.DefaultClient.Do(judgereq)
+
+	if err != nil {
+		return nil, fmt.Errorf("error sending request to Judge0: %v", err)
+
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %v", err)
+
+	}
+	
+	var submissions DashboardSubmissions
+	err = json.Unmarshal(body, &submissions)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling response: %v", err)
+
+	}
+	return submissions, nil
 }
