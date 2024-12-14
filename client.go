8fb9035ae49a77135d1b5c024ffb5923a1d9d76d
Soham Mahapatra <soham.mahapatra2005@gmail.com>
2024-12-14 22:41:43 +0530

    feat:added worker struct

--- client.go
+++ client.go
@@ -7,3 +7,19 @@ type Client struct {
 	httpClient   *http.Client
 	baseURL      string
 }
+
+type Worker struct {
+	Queue     string `json:"queue"`
+	Size      int    `json:"size"`
+	Available int    `json:"available"`
+	Idle      int    `json:"idle"`
+	Working   int    `json:"working"`
+	Paused    int    `json:"paused"`
+	Failed    int    `json:"failed"`
+}
+
+type Workers []Worker
+
+func (client *Client) GetWorkers() (Workers, error) {
+
+}
